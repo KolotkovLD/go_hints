7c2d32a56e97d03523f14aac32c9d23853e07de9
MacBaine <kolotkov93@gmail.com>
2024-10-19 09:33:34 +0300

    draft

---
+++ pipelines/poolOfWorkers.go
@@ -0,0 +1,18 @@
+package pipelines
+
+import (
+	"fmt"
+	"time"
+)
+
+// ### 1. **Пул воркеров**
+// Пул воркеров позволяет ограничить количество одновременно выполняющихся горутин, чтобы избежать перегрузки ресурсов. Задачи распределяются между воркерами для параллельной обработки.
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Printf("Воркер %d начал задачу %d\n", id, j)
+		time.Sleep(time.Second)
+		fmt.Printf("Воркер %d завершил задачу %d\n", id, j)
+		results <- j * 2
+	}
+}
---
+++ pipelines/poolOfWorkers_test.go
@@ -0,0 +1,26 @@
+package pipelines
+
+import (
+	"testing"
+)
+
+func TestWorker(t *testing.T) {
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	// Создаем 3 воркера
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	// Отправляем 5 задач
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	// Собираем результаты
+	for r := 1; r <= 5; r++ {
+		<-results
+	}
+}
